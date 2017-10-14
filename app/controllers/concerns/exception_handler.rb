fd2a42ff91efa5e531cec45f6ac17c2072267615
Felix Harrison <fmlharrison@gmail.com>
2017-10-14 19:58:37 +0100

    Added http exception handlers

---
+++ app/controllers/concerns/exception_handler.rb
@@ -0,0 +1,20 @@
+module ExceptionHandler
+  # provides the more gracefull included method
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      json_response({
+        message: e.message,
+        :not_found
+        })
+    end
+
+    rescue_from ActiveRecord::RecordInvalid do |e|
+      json_response({
+        message: e.message,
+        :unprocessable_entity
+        })
+    end
+  end
+end
